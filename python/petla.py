3a58914fdd8a05d68908b530ddf499fc61db7f1d
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2018-11-13 13:34:27 +0100

    Python petle

---
+++ python/petla.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+
+
+def main(args):
+    suma = 0 
+    for i in range(1, 101, 2):
+        print(suma)
+        suma += i
+        print() # wydruk pustego wiersza
+    print("Suma liczb <0;100>:", suma)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
