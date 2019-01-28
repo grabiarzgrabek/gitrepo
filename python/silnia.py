0f9a10b37b7a1c8aa26280c03960159b0b63502c
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2019-01-28 12:38:46 +0100

    potegi

--- python/silnia.py
+++ python/silnia.py
@@ -8,11 +8,16 @@
 # n! = 1 * ... * n
 # 4! = 1 * 2 * 4
 
-def main(args):
-    n=int(input("podaj n:"))
+def silnia_it(n):
     wynik = 1
     for i in range(1, n + 1):
-        wynik = wynik * i
+        wynik = wynik * 1
+    return wynik
+
+def main(args):
+    assert
+    n = int(input("podaj liczbe naturalna: "))
+    print 
         
     print(wynik)
     return 0
