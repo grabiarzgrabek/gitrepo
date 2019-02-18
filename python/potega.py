af5f70d2f58910a01db99fc4ef7e2ca0a7ed0d7a
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2019-02-18 12:36:39 +0100

    rekurencja

--- python/potega.py
+++ python/potega.py
@@ -2,28 +2,35 @@
 # -*- coding: utf-8 -*-
 #
 #  potega.py
-
+# 2^3 = 2 * 2 * 2
+# 4 ^ 4 = 4 * 4 * 4 * 4
 
 def czy_naturalna(a):
     if a.isdigit():
         return True
     return False
-    
+
+
 def potega_it(a, n):
     wynik = 1
     for i in range(n):
         wynik = wynik * a
     return wynik
-
+    
+def potega_rek(a, n):
+    # a^n = a^(n-1) * a
+    if n == 0:
+        return 1
 
 def main(args):
-    assert potega_it(2, 0) == 1
-    assert potega_it(2, 1) == 2
-    assert potega_it(0, 10) == 0
-    assert potega_it(1, 10) == 1
-    assert potega_it(3, 3) == 27
-    a = input("podaj podstawe: ")
-    n = input("podaj wykładnik: ")
+    assert potega_rek(2, 0) == 1
+    assert potega_rek(2, 1) == 2
+    assert potega_rek(0, 10) == 0
+    assert potega_rek(1, 10) == 1
+    assert potega_rek(3, 3) == 27
+    
+    a = input("Podaj podstawę: ")
+    n = input("Podaj wykładnik: ")
     if not czy_naturalna(a) or not czy_naturalna(n):
         print("Błędne dane!")
         return 0
@@ -32,4 +39,4 @@ def main(args):
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
--- python/silnia.py
+++ python/silnia.py
@@ -2,26 +2,33 @@
 # -*- coding: utf-8 -*-
 #
 #  silnia.py
-#
+#  
 # n! = 1 dla n = 0
-# n1 = n * (n-1)! dla n>= 1
+# n! = n * (n-1)! dla n >= 1
 # n! = 1 * ... * n
-# 4! = 1 * 2 * 4
+# 4! = 1 * 2 * 3 * 4
 
 def silnia_it(n):
     wynik = 1
     for i in range(1, n + 1):
-        wynik = wynik * 1
+        wynik = wynik * i
     return wynik
+    
+def silnia_rek(n):
+    if n == 0:
+        return 1
+    return silnia_rek(n - 1) * n
 
 def main(args):
-    assert
-    n = int(input("podaj liczbe naturalna: "))
-    print 
-        
-    print(wynik)
+    assert silnia_rek(0) == 1
+    assert silnia_rek(1) == 1
+    assert silnia_rek(2) == 2
+    assert silnia_rek(3) == 6
+
+    n = int(input("Podaj liczbę naturalną: "))
+    print("{}! = {}".format(n, silnia_rek(n)))
     return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
