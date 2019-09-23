9fd71d34ecca37df8766fda6145a1691359debad
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2019-09-23 09:02:51 +0200

    trójkąt

--- cpp/trojkat.cpp
+++ cpp/trojkat.cpp
@@ -9,31 +9,46 @@
  * 
  *  and (liczba1 + liczba3 > liczba2) and (liczba2 + liczba3 > liczba1)
  * 
+ * else {cout << "trójkąt o podanych bokach nie istnieje"<< endl;}
  */
 
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-    int liczba1;
-    int liczba2;
-    int liczba3;
+    int a;
+    int b;
+    int c;
 
 int main(int argc, char **argv)
 {   
     cout << "Podaj pierwszą liczbe: ";
-    cin >> liczba1;
+    cin >> a;
     cout << "Podaj drugą liczbe: ";
-    cin >> liczba2;
+    cin >> b;
     cout << "Podaj trzecią liczbe: ";
-    cin >> liczba3;
+    cin >> c;
     
-    if (liczba1 + liczba2 > liczba3) {
-    if (liczba1 + liczba3 > liczba2) {
-    if (liczba2 + liczba3 > liczba1) {
-        cout << "Podane liczby mogą tworzyć boki trójkąta";}
-	
+    if (a + b <= c) {
+    cout << "Trójkąt o podanych bokach nie istnieje";}
+    else if (a+c<=b){
+    cout << "Trójkąt o podanych bokach nie istnieje";}
+    else if (b+c<=a){
+    cout << "Trójkąt o podanych bokach nie istnieje";}
+    else {
+    cout << "Podane liczby mogą tworzyć boki trójkąta";}
+    
+    if(c==sqrt(a*a+b*b)){
+    cout << "i jest to trojkat prostokątny";}
+    else if(b==sqrt(a*a+c*c)){
+    cout << "i jest to trojkat prostokątny";}
+    else if(a==sqrt(b*b+c*c)){
+    cout << "i jest to trojkat prostokątny";}
+    else {
+    cout << ", nie jest to trójkąt prostokątny";}
+
 	return 0;
 }
 
