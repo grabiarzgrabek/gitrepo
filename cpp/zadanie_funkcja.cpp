5a5810aacfad760e2e75c3a63d0c711fd0490ca3
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2019-10-07 10:37:25 +0200

    funkcje2, zadanie_funkcje

---
+++ cpp/zadanie_funkcja.cpp
@@ -0,0 +1,24 @@
+/*
+ * zadanie_funkcja.cpp
+ * 
+ * Copyright 2019  <>
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+int staz = 1
+int zarobek = 1000
+void drukuj(){
+
+}
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
