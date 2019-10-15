8b065711b8d2a4e08e9e1d0bee84590e994e77ed
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2019-10-15 20:56:21 +0200

    .

--- cpp/tablica.cpp
+++ cpp/tablica.cpp
@@ -1,5 +1,7 @@
 /*
  * tablica.cpp
+ * 
+ * Copyright 2019  <>
  */
 
 
@@ -8,19 +10,19 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	int rozmiar = 5;
-    int tablica[rozmiar];
+    int rozmiar=5;
+	int tablica[rozmiar];
     int i = 0;
-    cout << "podaj 20 liczb: " << endl;
-    for (i = 0; i<rozmiar; i++) {
-        cin >> tablica[i];
-    }
-    for (i=0; i<rozmiar; i++) {
-        cout << tablica[i] << " ";
-    }
-    for (i=0; i<rozmiar; i++) {
-        cout << tablica[i] << " ";
+    cout<<"Podaj 20 liczb: "<<endl;
+    for(i=0;i<rozmiar;i++){
+        cin>>tablica[i];
+        }
+    for(i=0;i<rozmiar;i++){
+        cout<<tablica[i]<<" ";
+        }
+        cout<<endl;
+        for(i=0;i<rozmiar;i++){
+    cout<<tablica[i]<<" ";
     }
 	return 0;
 }
-
