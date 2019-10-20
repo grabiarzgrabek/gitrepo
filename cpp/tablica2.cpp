92fa94a3d07d57b92d63efe686a483dfff385241
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2019-10-20 22:14:44 +0200

    srednia

--- cpp/tablica2.cpp
+++ cpp/tablica2.cpp
@@ -1,41 +1,45 @@
 /*
  * tablica2.cpp
- * 
- * Copyright 2019  <>
  */
 
 
 #include <iostream>
 using namespace std;
 
-void pobierz0ceny(int t[], int r) {
-    cout << "podaj " << r << " ocen" << endl;
-    int i;
-    for (i=0; i < r; i++) {
-        cin >> t[i];
-    }
+void pobierzOceny(int t[], int r) {
+	cout << "Podaj " << r << " ocen: " << endl;
+	int i;
+	for (i=0; i < r; i++) {
+		cin >> t[i];
+	}
 }
+
 void drukujTab(int t[], int r) {
-    int i;
-    for(i=0; i<r; i++) {
-        cout << t[i];
-    }
+	int i;
+	for (i=0; i < r; i++) {
+		cout << t[i] << " ";
+	}
 }
-void srednia(int t[], int r) {
-    int i;
-    for(i=0; i<r; i++) {
-        cout << t[i];
+
+float srednia(int t[], int r) {
+	int i;
+	float srednia;
+    for (i=0; i < r; i++){
+		srednia += t[i];
     }
+    return srednia / r;
 }
 
 int main(int argc, char **argv)
 {
-    int rozmiar=5;
+	cout << "Podaj liczbe ocen: ";
+	int rozmiar;
+	cin >> rozmiar;
 	int tablica[rozmiar];
-    
-    pobierz0ceny(tablica, rozmiar);
-    drukujTab(tablica, rozmiar);
-    srednia(tablica, rozmiar)
-    
+
+	pobierzOceny(tablica, rozmiar);
+	drukujTab(tablica, rozmiar);
+    cout << "Srednia: " << srednia(tablica, rozmiar) << endl;
+
 	return 0;
 }
