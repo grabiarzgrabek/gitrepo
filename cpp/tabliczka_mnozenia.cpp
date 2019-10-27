b2ef69161c5a29535742a9e7c8092861115cdc66
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2019-10-27 23:39:56 +0100

    tabliczka mnozenia

---
+++ cpp/tabliczka_mnozenia.cpp
@@ -0,0 +1,30 @@
+/*
+ * tabliczka_mnozenia.cpp
+ */
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#define N	11
+#define M	11
+
+
+int main(int argc, char **argv)
+{
+	int tab2W[N][M];
+	int i, j;
+	for (i = 1;i < N; i++){
+		cout << i ;
+		for(j = 0; j < M; j++){
+			tab2W[i][j] = i * j;
+			cout << setw(4) << tab2W[i][j] << " ";
+			}
+		cout << endl;
+
+
+		}
+
+	return 0;
+}
