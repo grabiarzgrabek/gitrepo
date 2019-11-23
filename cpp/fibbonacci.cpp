8f75d18b00d70991a25a0971d797c28c0ffc088d
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2019-11-23 17:22:19 +0100

    ciąg

--- cpp/fibbonacci.cpp
+++ cpp/fibbonacci.cpp
@@ -1,29 +1,32 @@
 /*
- * fibbonacci.cpp
+ * fibonacci.cpp
  */
 
 
 #include <iostream>
 using namespace std;
 
-int fibbonacci_it(int n) {
-    int a = 0; //Fn-1
-    int b = 1; //Fn-2
-    int wynik = 0:
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    for (int i=2; i<n; i++) {
-        wynik = a + b;
-        cout << i << ': fib(" << b << ", " << a << ") = << wynik << endl;
-        b = a;
-        a = wynik;
-    }
+int fibonacci_it(int n) {
+	int a = 0; //Fn-1
+	int b = 1; //Fn-2
+	int wynik = 0;
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	for(int i = 0; i < n; i++) {
+		wynik = a + b; //1//2//3
+		b = a; //1//1//2
+		a = wynik; //1//2//3
+	}
+	return wynik;
 }
 
 int main(int argc, char **argv)
 {
-    fibbonacci_it(19);
-	
+	int ROZMIAR;
+	cout << "Ilosc liczb do wypisania: ";
+	cin >> ROZMIAR;
+	for (int i = 1; i < ROZMIAR + 1; i++){
+        cout << i << ": fib(" << fibonacci_it(i - 2) << ", " << fibonacci_it(i - 1) << ") = " << fibonacci_it(i) << endl;
+	}
 	return 0;
 }
-
