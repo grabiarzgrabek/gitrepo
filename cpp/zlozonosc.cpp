1dfc304db4f6e794c18b438faa8623c4a39cc6a8
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2020-01-09 11:40:05 +0100

    zlozonosc

---
+++ cpp/zlozonosc.cpp
@@ -0,0 +1,24 @@
+/*
+ * zlozonosc.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n = 5;
+    int tb[n] = {2, 4, 21, 37, 69};
+    int i = 0;
+    int x = 37;
+    for (i = 0; i < n; i++) {
+        if (tb[i] == x) {
+            cout << i;
+            return 0;
+        }
+    }
+    cout << -1;
+    return 0;
+}
+
