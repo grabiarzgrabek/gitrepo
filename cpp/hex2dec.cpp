e5712cf3f89078b8d751dc4742b6ed7e2a8dbe6f
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2020-02-10 10:37:44 +0100

    lipa

---
+++ cpp/hex2dec.cpp
@@ -0,0 +1,32 @@
+/*
+ * hex2dec.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int tab[4]
+    int x;
+    cout << "podaj znak w systemie szesnastkowym" << endl;
+    cin >> x;
+    if x = "a";
+    cout << 10 << endl;
+    if x == "b";
+    cout << 11 << endl;
+    if x == "c";
+    cout << 12 << endl;
+    if x == "d";
+    cout << 13 << endl;
+    if x == "e";
+    cout << 14 << endl;
+    if x == "f";
+    cout << 15 << endl;
+    else 
+    cout << x << endl;
+    
+	return 0;
+}
+
---
+++ cpp/jd.cpp
@@ -0,0 +1,18 @@
+/*
+ * dec2hex.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+    int liczba;
+    cout << "Podaj liczbe dziesiętnie: " << endl;
+    cin >> liczba;
+	std::cout<< hex << liczba;
+	return 0;
+}
+
