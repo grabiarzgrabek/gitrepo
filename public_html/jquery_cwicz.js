7d1ff63df7f06f5a2473f754abc36e4433e74e80
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2020-02-26 09:42:03 +0100

    stronka

--- public_html/jquery_cwicz.js
+++ public_html/jquery_cwicz.js
@@ -1,14 +1,18 @@
-$(document). ready(function() {
-  $(".wynik").text("Nowa tresc");
-  console.log(($("p").text()));
+$(document).ready(function(){
+  $(".wynik").text("Nowa treść");
+  console.log($("p").text());
   $("#przycisk").click(function(){
-     var vnk = parseInt($("#vnk").val());
-     var nk = parseInt($("#nk").val());
-     var R = vnk / nk * 100;
-     var div2 = '<div class="wspolczynnik">'+R+'</div>';
-     $("div.wynik").after(div2);
+    var vnk = parseInt($("#vnk").val());
+    var nk = parseInt($("#nk").val());
+    var R = vnk / nk * 100;
+    var div2 = '<div class="wspolczynnik">'+R+'</div>';
+    $("div.wynik").after(div2);
   });
   $("input").mouseover(function(){
-    $(this).addClass("tloBlue")
+    $(this).addClass("tłoBlue");
+  });
+  $("input").mouseout(function(){
+    $(this).removeClass("tłoBlue");
   });
 });
+  $()
