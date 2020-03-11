e0c53e7028bdcc52c9d7c6f701de17aac444f7a3
grabiarzgrabek <jakub.grabowski@lo1.sandomierz.pl>
2020-03-11 09:40:34 +0100

    rysowansko

---
+++ public_html/p5/empty-example/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(800, 600);
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  noFill();
+  strokeWeight(10);
+  stroke('blue');
+  ellipse(100, 100, 150, 150)
+  stroke('black');
+  ellipse(270, 100, 150, 150)
+  stroke('red');
+  ellipse(440, 100, 150, 150)
+  stroke('yellow');
+  ellipse(185, 200, 150, 150)
+  stroke('green');
+  ellipse(355, 200, 150, 150)
+  stroke('pink');
+  rect()
+}
